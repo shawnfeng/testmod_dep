1557cf9ebc34b877667a51798292e6f9f89701c1
Shawn Feng <fengguangxiang2015@ipalfish.com>
2018-12-20 16:32:46 +0800

    aa

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/shawnfeng/testmod_dep
 
-require github.com/shawnfeng/testmod v1.0.0
+require github.com/shawnfeng/testmod v1.0.2
--- t.go
+++ t.go
@@ -7,7 +7,7 @@ import (
 
 
 func Hidep(name string, arg string) string {
-	fmt.Println(testmod.Hi("testmod_dep mod roberto 1"))
+	fmt.Println(testmod.Hi("testmod_dep mod roberto 1", "AAA", "bbbb"))
 	return fmt.Sprintf("testmod_dep Hi dep, %s arg:%s", name, arg)
 }
 
